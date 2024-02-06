d3a087ce77c6694d238be56590bbb8d8d83a1c7a
arthurvini98 <118913045+arthurvini98@users.noreply.github.com>
2024-02-06 20:40:56 -0300

    Update main.py
    Adicionado as alterações propostas pela professora Raquel.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import numpy as np
-import os
+from pathlib import Path
+
+def list_files_in_directory(directory):
+    files = list(directory.glob('*'))
+    files_dict = {i: file.name for i, file in enumerate(files, start=1)}
+    return files_dict
 
 def ler_matriz_distancias(arquivo):
-    return np.loadtxt(arquivo, dtype='int')
+    matriz_distancias = np.loadtxt(arquivo, dtype='int')
+    tamanho_matriz = len(matriz_distancias)
+    return matriz_distancias, tamanho_matriz
 
 def vizinho_mais_proximo(matriz_distancias, ponto_inicial):
     numero_de_cidades = len(matriz_distancias)
@@ -30,15 +37,29 @@ def vizinho_mais_proximo(matriz_distancias, ponto_inicial):
     return visitados, custo_total
 
 def principal():
-    diretorio_script = os.path.dirname(os.path.realpath(__file__))
-    arquivo = os.path.join(diretorio_script, 'distancia.txt')
-    ponto_inicial = int(input('Informe o ponto inicial [0-14]: '))
-    
-    matriz_distancias = ler_matriz_distancias(arquivo)
-    caminho, custo = vizinho_mais_proximo(matriz_distancias, ponto_inicial)
-    
-    print('Caminho percorrido:', caminho)
-    print('Custo total:', custo)
+    src_dir = Path(__file__).resolve().parent / 'bases'
+    files_dict = list_files_in_directory(src_dir)
+
+    for index, file in files_dict.items():
+        print(f"{index}: {file}")
+
+    try:
+        file_index = int(input('Selecione o índice da base de sua escolha: '))
+        file_name = files_dict[file_index]
+        file_path = src_dir / file_name
+    except (ValueError, KeyError):
+        print("Índice inválido.")
+        return
+
+    if file_path.exists() and file_path.is_file():
+        matriz_distancias, tamanho_matriz = ler_matriz_distancias(file_path)  # Obtendo a matriz de distâncias e o tamanho
+        ponto_inicial = int(input(f'Informe o ponto inicial [0-{tamanho_matriz - 1}]: '))
+        caminho, custo = vizinho_mais_proximo(matriz_distancias, ponto_inicial)
+        print('Caminho percorrido:', caminho)
+        print('Custo total:', custo)
+        
+    else:
+        print(f"Arquivo '{file_name}' não encontrado no diretório '{src_dir}'.")
 
 if __name__ == '__main__':
     principal()
