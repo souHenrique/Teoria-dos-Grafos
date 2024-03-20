0ab814a2f35c40a7a9cc2f5ea8380b02086ed442
Luis Henrique <128126715+souHenrique@users.noreply.github.com>
2024-03-20 16:35:53 -0300

    dijkstra.py
    alterações

--- dijkstra.py
+++ dijkstra.py
@@ -1,41 +1,33 @@
 import heapq
 
-optimal_tour_length = {
-    1: 33523,
-    2: 699,
-    3: 19,
-    4: 937,
-    5: 2085,
-    6: 291
-}
 
 def dijkstra(graph, start, file_index):
     distancia_total = 0
-    distancia = {vertex: float('inf') for vertex in range(1, len(graph) + 1)}
-    distancia[start] = 0
-    queue_de_prioridade = [(0, start)]
+    distancias = {vertice: float('inf') for vertice in range(1, len(graph) + 1)}
+    distancias[start] = 0
+    fila_de_prioridade = [(0, start)]
 
-    tamanho_otimo = optimal_tour_length.get(file_index)
-    threshold_distance = tamanho_otimo * 1.5
+    antecessores = {vertice: None for vertice in range(1, len(graph) + 1)}
+    path = []
 
-    while queue_de_prioridade:
-        current_distance, current_vertex = heapq.heappop(queue_de_prioridade)
+    while fila_de_prioridade:
+        distancia_atual, vertice_atual = heapq.heappop(fila_de_prioridade)
 
-        if current_distance > distancia[current_vertex]:
+        if distancia_atual > distancias[vertice_atual]:
             continue
 
-        distancia_total += current_distance
+        distancia_total += distancia_atual
 
-        if distancia_total > threshold_distance:
-            break
+        if vertice_atual != start:
+            path.append(vertice_atual)
 
-        for vizinho, peso in enumerate(graph[current_vertex], start=1):
+        for vizinho, peso in enumerate(graph[vertice_atual], start=1):
             if peso > 0:
-                distancia = current_distance + peso
-                if distancia < distancia[vizinho]:
-                    distancia[vizinho] = distancia
-                    heapq.heappush(queue_de_prioridade, (distancia, vizinho))
+                distancia = distancia_atual + peso
+                if distancia < distancias[vizinho]:
+                    distancias[vizinho] = distancia
+                    antecessores[vizinho] = vertice_atual
+                    heapq.heappush(fila_de_prioridade, (distancia, vizinho))
 
-    porcentagem_da_diferenca = abs(distancia_total - tamanho_otimo) / tamanho_otimo * 100
-
-    print(f"Peso total: {distancia_total}\nValor esperado: {tamanho_otimo}\nMargem de erro: {porcentagem_da_diferenca:.3f}%")
+    print("Caminho percorrido:", [start] + path)
+    print(f"Peso total do caminho: {distancia_total}")
